c7e633847427be19e68d7168192b21c4af39be75
Adloya <34348778+Adloya@users.noreply.github.com>
2022-06-06 17:08:24 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,17 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/css/main.css", "./src/js/pages.js", "./assets/images/logo192.png", "./assets/images/logo512.png", "./assets/images/logoPdubois-full.png"]);
-        })
-    )
-})
+// self.addEventListener("install", e => {
+//     e.waitUntil(
+//         caches.open("static").then(cache => {
+//             return cache.addAll(["./", "./src/css/main.css", "./src/js/pages.js", "./assets/images/logo192.png", "./assets/images/logo512.png", "./assets/images/logoPdubois-full.png"]);
+//         })
+//     )
+// })
 
 self.addEventListener("fetch", e => {
     e.respondWith(
-        caches.match(e.request).then(response => {
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "./src/css/main.css", "./src/js/pages.js", "./assets/images/logo192.png", "./assets/images/logo512.png", "./assets/images/logoPdubois-full.png"]);
+        }) || caches.match(e.request).then(response => {
+            console.log(response)
             return response || fetch(e.request);
         })
     )
